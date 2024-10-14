2f2c769e1f4e2c98551902111fc5c25f00fac44e
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-10-14 18:50:44 +0530

    added array

---
+++ array.js
@@ -0,0 +1,7 @@
+// using square brackets
+var numbers = [1, 2, 3, 4, 5];
+// using array constructor
+var arr = new Array(1, 2, 3, 4, 5);
+console.log(numbers);
+console.log(arr);
+console.log(numbers[2]);
---
+++ array.ts
@@ -0,0 +1,11 @@
+// using square brackets
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+
+// using array constructor
+const arr: number[] = new Array(1, 2, 3, 4, 5);
+
+
+console.log(numbers);
+console.log(arr);
+console.log(numbers[2]);
