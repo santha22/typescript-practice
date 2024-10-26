64f8d731b304e7d1d109d8397abcbda3c5a9d509
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-10-26 13:44:36 +0530

    practice in getter and setter

--- dist/getter_setter.js
+++ dist/getter_setter.js
@@ -1,27 +1,66 @@
 "use strict";
-class Persons1 {
-    constructor(name, hobbies) {
-        this.name = name;
-        this.hobbies = hobbies;
+// class Persons1 {
+//     private _age: number | undefined;
+//     constructor(public name: string, protected hobbies: string[]) {}
+//     public set age(age: number) {
+//         if (age > 150 || age < 0) {
+//             throw new Error("age is not valid");
+//         } 
+//         this._age = age;
+//     }
+//     public get age() : number {
+//         if(this._age ===  undefined) {
+//             throw new Error("age is not valid");
+//         } 
+//         return this._age;
+//     }
+//     introduceParent(): string {
+//         return `Hi, I'm ${this.name} and I'm
+//             ${this._age} years old. I love ${this.hobbies.join(",")}.`;
+//     }
+// }
+// const person5: Persons1 = new Persons1("santha", ["reading", "painting"]);
+// person5.age = 12;
+// console.log(person5.introduceParent());
+// console.log(person5.age);
+// class BankAccount {
+//     private _balance: number = 0;
+//     public set balance(newBalance: number) {
+//         if(newBalance < 0){
+//             throw new Error("negative balance");
+//         }
+//         this._balance = newBalance;
+//     }
+//     public get balance(): number {
+//         if(this._balance < 0){
+//             throw new Error("negative balance");
+//         }
+//         return this._balance;
+//     }
+// }
+// const account = new BankAccount();
+// account.balance = 200;
+// console.log(account.balance);
+// Temperature Converter 
+class Temperature {
+    constructor() {
+        this._celsius = 0;
     }
-    set age(age) {
-        if (age > 150 || age < 0) {
-            throw new Error("age is not valid");
-        }
-        this._age = age;
+    get celsius() {
+        return this._celsius;
     }
-    get age() {
-        if (this._age === undefined) {
-            throw new Error("age is not valid");
-        }
-        return this._age;
+    set celsius(newCelsius) {
+        this._celsius = newCelsius;
     }
-    introduceParent() {
-        return `Hi, I'm ${this.name} and I'm
-            ${this._age} years old. I love ${this.hobbies.join(",")}.`;
+    get fahrenheit() {
+        return (this._celsius * 9) / 5 + 32;
+    }
+    set fahrenheit(newFa) {
+        this._celsius = ((newFa - 32) * 5) / 9;
     }
 }
-const person5 = new Persons1("santha", ["reading", "painting"]);
-person5.age = 12;
-console.log(person5.introduceParent());
-console.log(person5.age);
+const temp = new Temperature();
+temp.celsius = 25;
+console.log(temp.fahrenheit);
+temp.fahrenheit = 77;
+console.log(temp.celsius);
--- src/getter_setter.ts
+++ src/getter_setter.ts
@@ -1,32 +1,91 @@
-class Persons1 {
-    private _age: number | undefined;
+// class Persons1 {
+//     private _age: number | undefined;
 
-    constructor(public name: string, protected hobbies: string[]) {}
+//     constructor(public name: string, protected hobbies: string[]) {}
 
-    public set age(age: number) {
-        if (age > 150 || age < 0) {
-            throw new Error("age is not valid");
-        } 
-        this._age = age;
+//     public set age(age: number) {
+//         if (age > 150 || age < 0) {
+//             throw new Error("age is not valid");
+//         } 
+//         this._age = age;
+//     }
+
+//     public get age() : number {
+//         if(this._age ===  undefined) {
+//             throw new Error("age is not valid");
+//         } 
+//         return this._age;
+//     }
+
+//     introduceParent(): string {
+//         return `Hi, I'm ${this.name} and I'm
+//             ${this._age} years old. I love ${this.hobbies.join(",")}.`;
+//     }
+// }
+
+// const person5: Persons1 = new Persons1("santha", ["reading", "painting"]);
+
+// person5.age = 12;
+
+// console.log(person5.introduceParent());
+// console.log(person5.age);
+
+
+
+// class BankAccount {
+//     private _balance: number = 0;
+
+//     public set balance(newBalance: number) {
+//         if(newBalance < 0){
+//             throw new Error("negative balance");
+//         }
+//         this._balance = newBalance;
+//     }
+
+//     public get balance(): number {
+//         if(this._balance < 0){
+//             throw new Error("negative balance");
+//         }
+//         return this._balance;
+//     }
+// }
+
+// const account = new BankAccount();
+// account.balance = 200;
+
+// console.log(account.balance);
+
+
+
+
+// Temperature Converter 
+class Temperature {
+    private _celsius: number = 0;
+
+    public get celsius(): number {
+        return this._celsius;
     }
 
-    public get age() : number {
-        if(this._age ===  undefined) {
-            throw new Error("age is not valid");
-        } 
-        return this._age;
+    public set celsius(newCelsius: number) {
+        this._celsius = newCelsius;
     }
 
-    introduceParent(): string {
-        return `Hi, I'm ${this.name} and I'm
-            ${this._age} years old. I love ${this.hobbies.join(",")}.`;
+    public get fahrenheit():number {
+        return (this._celsius * 9) / 5 + 32
     }
+
+    public set fahrenheit(newFa: number) {
+        this._celsius = ((newFa - 32) * 5) / 9;
+    }
+
 }
 
-const person5: Persons1 = new Persons1("santha", ["reading", "painting"]);
 
-person5.age = 12;
+const temp = new Temperature();
+temp.celsius = 25;
 
-console.log(person5.introduceParent());
-console.log(person5.age);
+console.log(temp.fahrenheit);
 
+
+temp.fahrenheit = 77;
+console.log(temp.celsius);
